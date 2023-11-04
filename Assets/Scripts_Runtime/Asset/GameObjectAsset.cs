7dc902b32b883b1f30e024712148578913ece8d2
刘永祺 <liuyongqi@xmfunny.com>
2023-11-04 11:15:38 +0800

    <feature> 补充资源加载方法

--- Assets/Scripts_Runtime/Asset/GameObjectAsset.cs
+++ Assets/Scripts_Runtime/Asset/GameObjectAsset.cs
@@ -1,9 +1,9 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace BusinessAlgorithm.Asset {
     public class GameObjectAsset {
@@ -23,9 +23,32 @@ namespace BusinessAlgorithm.Asset {
             }
         }
 
-        public bool TryGet(string name, out GameObject go) {
-            bool has = all.TryGetValue(name, out go);
-            return has;
+        public async Task<GameObject> LoadAssetSync(string resourceName) {
+            var go = await Addressables.LoadAssetAsync<GameObject>(resourceName).Task;
+            return go;
+        }
+
+        public void LoadAssetAsync(string resourceName, Action<GameObject> callback) {
+            bool has = all.TryGetValue(resourceName, out GameObject go);
+            if (has) {
+                callback?.Invoke(go);
+                return;
+            }
+
+            AsyncOperationHandle<GameObject> handle = Addressables.LoadAssetAsync<GameObject>(resourceName);
+
+            handle.Completed += (operation) => OnLoadComplete(operation, resourceName, callback);
+        }
+
+        void OnLoadComplete(AsyncOperationHandle<GameObject> handle, string resourceName, Action<GameObject> callback) {
+            if (handle.Status == AsyncOperationStatus.Succeeded) {
+                callback?.Invoke(handle.Result);
+            } else {
+                // 处理加载失败的情况
+                Debug.LogError("Failed to load addressable: " + resourceName);
+            }
+
+            Addressables.Release(handle);
         }
 
         public void ClearAll() {
